33017e46eaf739fe0980547d0f933528f820ad3e
Nehuen Melo <nehuenmelo@gmail.com>
2021-01-08 18:51:39 -0300

    CRUD con Agregar, Mostrar all, Actualizar y Borrar

--- index.js
+++ index.js
@@ -12,6 +12,7 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 
 app.use('/api', apiRouter);
+
  
 app.listen(3000 , () => {
     console.log('Servidor arrancado')
--- routes/api/operations.js
+++ routes/api/operations.js
@@ -1,5 +1,35 @@
 const router = require('express').Router();
 
+const { Operation } = require('../../db');
+
+// GET /api/operations - recupera todas las operaciones 
+router.get('/', async (req, res) => {
+    const operations = await Operation.findAll();
+    res.json(operations);
+});
+
+// POST /api/operations - crea una operacion
+router.post('/', async (req, res) => {
+    const operation = await Operation.create(req.body);
+    res.json(operation);
+});
+
+// PUT /api/operations/4 - edita la operacion 4
+router.put('/:operationId', async (req, res) => {
+    await Operation.update(req.body, {
+        //filtro que queremos hacer sobre los datos de la tabla(cual quiero modificar)
+        where: { id: req.params.operationId }
+    });
+    req.json({success: 'Se ha modificado'});
+});
+
+// DELETE /api/operations/4 - borra la operacion 4
+router.delete('/:operationId', async(req, res) => {
+    await Operation.destroy({
+        where: { id: req.params.operationId }
+    });
+    res.json({ success: 'Se ha borrado correctamente la operacion'});
+});
 
 
 module.exports = router;
